97ba0959a90685b43164bc995cff5258fde1d3c1
Jonathan Gouws <jonathan.gouws@gmail.com>
2022-03-29 10:10:43 +0200

    Finished Slides etc.

--- ML/Assignment2/task2/JLGouws19G4436task2.py
+++ ML/Assignment2/task2/JLGouws19G4436task2.py
@@ -18,7 +18,7 @@ wine_df = pd.read_csv('winequality-red.csv')
 
 print("Number of missing entries per feature before cleaning:")
 print(wine_df.isnull().sum())
-wine_df.fillna(wine_df.median(), inplace = True)
+wine_df.fillna(wine_df.median(), inplace = True)#fill in missing data
 print("Number of missing entries after cleaning: ", end ="")
 print(wine_df.isnull().sum().sum())
 
@@ -26,6 +26,7 @@ X = wine_df.drop('quality', axis=1).values
 y = np.ravel(wine_df[['quality']])
 
 #split the data here do not look at test values
+#30% test data
 X_train, X_test, y_train, y_test = mselect.train_test_split(X, y, test_size=0.3, random_state=42)
 wine_train_df = pd.DataFrame(np.c_[X_train, y_train], columns=wine_df.columns)
 print("Training Dataframe")
@@ -53,26 +54,42 @@ plt.yticks(fontsize=15)
 scalerList = [("FunctionTransformer", "No Scaling"), ("MinMaxScaler", "Min Max Scaler"), 
               ("RobustScaler", "Min Max Scaler"), ("StandardScaler", "Standard Scaler"), 
               ("Normalizer", "Normalizer")]
+print()
+print("For unbinned data")
+print()
 
+#The standard scaler appears to be marginally better in this case.
 for scalerTup in scalerList:
     scaler = getattr(prep, scalerTup[0])()
     transformer = scaler.fit(X_train)    # fit data for scaler
     scaledData = transformer.transform(X_train)
-    knnEstimator = knn(n_neighbors = 1)
+    #create a knn estimator
+    knnEstimator = knn(n_neighbors = 3)
+    #do a cross validation
     cvresult = mselect.cross_validate(estimator = knnEstimator, X = X_train, 
             y = y_train, cv = 5, n_jobs = -1, 
             scoring = ('accuracy', 'f1_weighted')) #cross validate with required metrics
-    print(scalerTup[1] ,cvresult['test_accuracy'],cvresult['test_f1_weighted'])
+    print("The results of cross validation")
+    print("Scaler", scalerTup[1], " -- Accuracy: ", cvresult['test_accuracy'], " -- f1 score: ", cvresult['test_f1_weighted'])
+    print()
 
     scaledTest = transformer.transform(X_test)
     y_pred = knn(n_neighbors = 1).fit(scaledData, y_train).predict(scaledTest)
     print(scalerTup[1])
+    #print the classification report
+    print()
+    print("The results of testing")
     print(metrics.classification_report(y_test, y_pred, zero_division = 0))
+    print()
     reportDict = metrics.classification_report(y_test, y_pred, zero_division = 0, output_dict = True)
     reportDict['accuracy'] = {'precision': ' ', 'recall': ' ', 'f1-score': reportDict['accuracy'], 'support': reportDict['weighted avg']['support']}
+    #this is incase I want to put the reports into LaTeX. I didn't have enough time to do it
     reportDf = pd.DataFrame(data = reportDict).transpose()
-    print(reportDf)
+    #print(reportDf)
 
+###############################################################################
+#Task 5
+#Assign the quality to different bins
 y_train_string = y_train.astype(str)
 y_train_string[np.any([y_train == 3,  y_train == 4], axis = 0)] = "bad"
 y_train_string[np.any([y_train == 5,  y_train == 6], axis = 0)] = "average"
@@ -83,6 +100,11 @@ y_test_string[np.any([y_test == 3,  y_test == 4], axis = 0)] = "bad"
 y_test_string[np.any([y_test == 5,  y_test == 6], axis = 0)] = "average"
 y_test_string[np.any([y_test == 7,  y_test == 8], axis = 0)] = "good"
 
+print()
+print("For the binned data")
+print()
+
+#Copied and pasted the above code
 for scalerTup in scalerList:
     scaler = getattr(prep, scalerTup[0])()
     transformer = scaler.fit(X_train)    # fit data for scaler
@@ -91,13 +113,19 @@ for scalerTup in scalerList:
     cvresult = mselect.cross_validate(estimator = knnEstimator, X = X_train, 
             y = y_train_string, cv = 5, n_jobs = -1, 
             scoring = ('accuracy', 'f1_weighted')) #cross validate with required metrics
+
+    print("The results of cross validation")
     print(scalerTup[1] ,cvresult['test_accuracy'],cvresult['test_f1_weighted'])
+    print()
 
     scaledTest = transformer.transform(X_test)
     y_pred_string = knn(n_neighbors = 1).fit(scaledData, y_train_string).predict(scaledTest)
     print(scalerTup[1])
+
+    print()
+    print("The results of testing")
     print(metrics.classification_report(y_test_string, y_pred_string, zero_division = 0))
     reportDict = metrics.classification_report(y_test_string, y_pred_string, zero_division = 0, output_dict = True)
     reportDict['accuracy'] = {'precision': ' ', 'recall': ' ', 'f1-score': reportDict['accuracy'], 'support': reportDict['weighted avg']['support']}
     reportDf = pd.DataFrame(data = reportDict).transpose()
-    print(reportDf)
+    #print(reportDf)
