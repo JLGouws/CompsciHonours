bb0915ee569d2d551fe33e63032542eb45f11411
Jonathan Gouws <jonathan.gouws@gmail.com>
2022-04-12 23:12:06 +0200

    Did some more work on ml assignment 3 task4

--- ML/Assignment3/task4/multi_classifier.py
+++ ML/Assignment3/task4/multi_classifier.py
@@ -1,23 +1,81 @@
 import argparse
 import numpy as np
 from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.decomposition import PCA
 from sklearn.utils import Bunch
 from pathlib import Path
 import skimage
 from skimage.io import imread
 from skimage.transform import resize
+from sklearn.pipeline import Pipeline
 import pandas as pd
+import sklearn.preprocessing as prep
 
 
 random_state = 42
 
 scaler = None
 
+def pca_map(X, figsize=(10,10), sup="", print_values= False):
+    import matplotlib.pyplot as plt
+    #PCA
+    columns=X.columns.values
+    pca=PCA(n_components=2)
+    pca.fit(X)
+    pca_values=pca.components_
+    
+    #Plot
+    plt.figure(figsize=figsize)
+    plt.rcParams.update({'font.size': 14}) 
+    
+    #Plot circle
+    x=np.linspace(start=-1,stop=1,num=500)
+    y_positive=lambda x: np.sqrt(1-x**2) 
+    y_negative=lambda x: -np.sqrt(1-x**2)
+    plt.plot(x,list(map(y_positive, x)), color='maroon')
+    plt.plot(x,list(map(y_negative, x)),color='maroon')
+    
+    #Plot smaller circle
+    x=np.linspace(start=-0.5,stop=0.5,num=500)
+    y_positive=lambda x: np.sqrt(0.5**2-x**2) 
+    y_negative=lambda x: -np.sqrt(0.5**2-x**2)
+    plt.plot(x,list(map(y_positive, x)), color='maroon')
+    plt.plot(x,list(map(y_negative, x)),color='maroon')
+    
+    #Create broken lines
+    x=np.linspace(start=-1,stop=1,num=30)
+    plt.scatter(x,[0]*len(x), marker='_',color='maroon')
+    plt.scatter([0]*len(x), x, marker='|',color='maroon')
+
+    #Define color list
+    colors = ['blue', 'red', 'green', 'black', 'purple', 'brown']
+    if len(pca_values[0]) > 6:
+        colors=colors*(int(len(pca_values[0])/6)+1)
+
+    #Plot arrow
+    add_string=""
+    for i in range(len(pca_values[0])):
+        xi=pca_values[0][i]
+        yi=pca_values[1][i]
+        plt.arrow(0,0, 
+                  dx=xi, dy=yi, 
+                  head_width=0.03, head_length=0.03, 
+                  color=colors[i], length_includes_head=True)
+        if print_values==True:
+            add_string=f" ({round(xi,2)} {round(yi,2)})"
+        plt.text(pca_values[0, i], 
+                 pca_values[1, i] , 
+                 s=columns[i] + add_string )
+
+    plt.xlabel(f"Component 1 ({round(pca.explained_variance_ratio_[0]*100,2)}%)")
+    plt.ylabel(f"Component 2 ({round(pca.explained_variance_ratio_[1]*100,2)}%)")
+    plt.title('Variable factor map (PCA)')
+    plt.suptitle(sup, y=1, fontsize=18)
+    plt.show()
+
 def preprocessImages(X, images):
-    global scaler
     from skimage.feature import hog
     from skimage.color import rgb2gray
-    import sklearn.preprocessing as prep
     if (images.shape[1] >= 25 and images.shape[2] >= 25):
         data = []
         for image in images:
@@ -25,17 +83,23 @@ def preprocessImages(X, images):
         X = np.array(data)
     elif len(images.shape) - 1 == 3:
         X = rgb2gray(images)
-    if scaler == None:
-        scaler = prep.MinMaxScaler().fit(X)
-    X = scaler.transform(X)
     return X
 
 def preprocessNonImages(X):
     return X
 
 def preprocess_data(X, images = None):
-    if(images is not None): return preprocessImages(X, images)
-    else: return preprocessNonImages(X)
+    global preprocess_data
+    X = preprocessImages(X, images) if(images is not None) else preprocessNonImages(X)
+    scaler = Pipeline([('PCA', PCA(n_components = 0.99, svd_solver = 'full')), ('scaler', prep.MinMaxScaler())])
+    scaler.fit(X)
+    X = scaler.transform(X)
+    def new_prep(X, images = None):
+        X = preprocessImages(X, images) if(images is not None) else preprocessNonImages(X)
+        X = scaler.transform(X)
+        return X
+    preprocess_data = new_prep
+    return X
 
 def k_nearest(X, y):
     from sklearn.neighbors import KNeighborsClassifier as knn
@@ -44,7 +108,7 @@ def k_nearest(X, y):
         {'n_neighbors': [1, 2, 3, 5, 7, 10, 15, 20]},
     ]
 
-    grid = GridSearchCV(knn(), tune_param, cv=5, scoring='f1_weighted')
+    grid = GridSearchCV(knn(), tune_param, cv=5, scoring='f1_weighted', n_jobs = -1)
 
     grid.fit(X, y)
 
@@ -54,12 +118,21 @@ def k_nearest(X, y):
 
 def logistic_regression(X, y):
     from sklearn.linear_model import LogisticRegression as LR
-    model = LR(random_state = random_state)
-    model.fit(X, y)
+
+    tune_param =[
+            {'C': [1e-2, 1e-1, 1, 5, 10, 20], 'penalty': ['l1', 'l2'], 'solver' : ['saga', 'liblinear']},
+    ]
+
+    grid = GridSearchCV(LR(random_state = random_state, max_iter = 3000, warm_start = True)
+            , tune_param, cv=5, scoring='f1_weighted', n_jobs = -1)
+
+    grid.fit(X, y)
+
+    model = grid.best_estimator_ #get the best model predicted by the grid search
     return model
 
 def random_forest(X, y):
-    from sklearn.ensemble import RandomForestClassifer as RFC
+    from sklearn.ensemble import RandomForestClassifier as RFC
     model = RFC(random_state = random_state)
     model.fit(X, y)
     return model
@@ -82,7 +155,7 @@ def svm(X, y):
         {'kernel': ['rbf', 'poly', 'sigmoid'], 'gamma': [1e-1, 1e-2, 1e-3], 'C': [0.01, 1, 10]},
     ]
 
-    grid = GridSearchCV(SVC(), tune_param, cv=5, scoring='f1_weighted')
+    grid = GridSearchCV(SVC(), tune_param, cv=5, scoring='f1_weighted', n_jobs = -1)
 
     grid.fit(X, y) #note how we do CV on the training set
 
@@ -106,7 +179,7 @@ def mlp(X, y):
         }
     ]
 
-    grid = GridSearchCV(mlp, tune_param, cv=5, scoring='f1_weighted')
+    grid = GridSearchCV(mlp, tune_param, cv=5, scoring='f1_weighted', n_jobs = -1)
 
     grid.fit(X, y)
 
@@ -165,8 +238,20 @@ def score_metrics(model, X_test, y_test):
 
 
 def compare_models(models, X_test, y_test):
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+    model_dict = {'Model' : args.classifiers.split(','), 'Precision' : [], 'Accuracy' : [], 'Recall' : []}
     for model in models:
-        print(score_metrics(model, X_test, y_test))
+        precision, accuracy, recall = score_metrics(model, X_test, y_test)
+        model_dict['Precision'] += [precision]
+        model_dict['Accuracy'] += [accuracy]
+        model_dict['Recall'] += [recall]
+    fig, axes = plt.subplots(3, 1, figsize = (len(model_dict['Model']) * 5, 21))
+    results_df = pd.DataFrame(model_dict)
+    sns.barplot(x="Model", y="Precision", data = results_df, ax = axes[0])
+    sns.barplot(x="Model", y="Accuracy", data = results_df, ax = axes[1])
+    sns.barplot(x="Model", y="Recall", data = results_df, ax = axes[2])
+    plt.savefig("ModelsCompared.pdf")
     return
 
 def visualize_images(df, images):
@@ -200,6 +285,7 @@ def visualize_non_images(df):
 
         plt.xticks(rotation=30)
 
+    pca_map(df.drop('Class', axis = 1))
     plt.show()
     return
 
@@ -207,6 +293,11 @@ def visualize_data(df, images = None):
     if(images is not None): visualize_images(df, images)
     else: visualize_non_images(df)
     return
+
+def encode_kddcup99(dataset):
+    from sklearn.feature_extraction.text import CountVectorizer
+    X = prep.OneHotEncoder(handle_unknown='ignore').fit_transform(dataset.data)
+    return X
 
 # Entry Point of Program
 if __name__ == '__main__':
@@ -225,10 +316,16 @@ if __name__ == '__main__':
             from sklearn import datasets
             dataset = getattr(datasets, 'load_' + args.dataset)()
         except Exception as e:
-            print(e)
+            try:
+                dataset = getattr(datasets, 'fetch_' + args.dataset)()
+            except Exception as e1:
+                print(e, e1, sep='\n')
 
     X = dataset.data
     y = dataset.target
+
+    if args.dataset == 'kddcup99':
+        X = encode_kddcup99(dataset)
 
     # Split Dataset
     X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.3,random_state=random_state)
@@ -236,7 +333,9 @@ if __name__ == '__main__':
     train_df = pd.DataFrame(X_train, columns = dataset.feature_names)
     train_df['Class'] = dataset.target_names[y_train]
 
-    train_images, test_images = train_test_split(dataset.images, y, test_size=0.3, random_state=random_state)[0:2] if hasattr(dataset, 'images') else None
+    train_images, test_images = None, None
+    if hasattr(dataset, 'images'):
+        train_images, test_images = train_test_split(dataset.images, y, test_size=0.3, random_state=random_state)[0:2]  
     # Visualize Data
 
     visualize_data(train_df, train_images)
