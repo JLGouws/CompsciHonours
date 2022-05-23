e920224de1d3ad72c888fc5babcdab5c5b6d67e0
J L Gouws <jonathan.gouws@gmail.com>
2022-05-23 12:44:35 +0200

    Did work on prac1 of GPUs

--- GPU/prac1/MCPi.cu
+++ GPU/prac1/MCPi.cu
@@ -1,30 +1,90 @@
 #include <cuda_runtime.h>
 #include "helper_cuda.h"
+#include "device_launch_parameters.h"
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+#include <iostream>
 #include <string.h>
 #include <curand_kernel.h>
-#define SEED 35791246
-#define ITER 10000000
+#define SEED 1234 
+#define ITER 10000
+#define EPBLOCK 16
 
-__global__ void monte_carlo(const int iterThread)
+using namespace std;
+
+__global__ void setup_rng_states(curandState *state, const int n)
 {
-    curandState state;
-    unsigned int id = threadIdx.x + blockIdx.x * blockDim.x, count = 0;
-    float x, y;
-
-    curand_init(SEED, 2 * id, 0, &state);
-    for(int i = 0; i < iterThread; i++){
-      x = curand_uniform(&state);
-      y = curand_uniform(&state);
-      count += (x * x + y * y) <=1;
-    }
+  unsigned int id = threadIdx.x + blockIdx.x * blockDim.x;
+  unsigned int stride = gridDim.x * blockDim.x;
+  for(; id < n; id += stride) curand_init(SEED, id * 7, 0, &state[id]);
 }
 
-int main(int argc, char* argv)
+__global__ void generate_coordinates(curandState *state, float *x,
+                                                          float *y, const int n)
 {
+  unsigned int id = threadIdx.x + blockIdx.x * blockDim.x;
+  unsigned int stride = gridDim.x * blockDim.x;
+  curandState localState = state[id];
+  for(; id < n; id += stride){
+    x[id] = curand_uniform(&localState);
+    y[id] = curand_uniform(&localState);
+  }
+}
+
+__global__ void monte_carlo(float *x, 
+                                                          float *y, const int n)
+{
+  unsigned int id = threadIdx.x + blockIdx.x * blockDim.x,
+               stride = gridDim.x * blockDim.x,
+               count = 0;
+
+  float xl, yl;
+
+  printf("hi");
+  for(; id < n; id += stride){
+    xl = x[id];
+    yl = y[id];
+    printf("(%.f, %.f)", xl, yl);
+    count += (xl * xl + yl * yl) <=1;
+  }
+}
+
+void checkerror(cudaError err)
+{ if (err != cudaSuccess) cout << cudaGetErrorString(err) << endl;
+	return;
+}
+
+int main()//int argc, char* argv)
+{
+  unsigned int threadsPerBlock = 256, BlocksPerGrid;
+  float *x, *y;
+
+  BlocksPerGrid = (ITER + threadsPerBlock - 1) / threadsPerBlock / EPBLOCK;
+  curandState *devStates;
+
+  //space for state rng states
+  cudaMalloc((void **) &devStates, threadsPerBlock * BlocksPerGrid 
+                                                        * sizeof(curandState));
+
+  cudaMalloc((void **) &x, ITER * sizeof(float));
+
+  cudaMalloc((void **) &y, ITER * sizeof(float));
+
+
+  cout << BlocksPerGrid << endl;
+  cout << threadsPerBlock << endl;
+  setup_rng_states<<<BlocksPerGrid, threadsPerBlock>>>(devStates, ITER);
+  checkerror(cudaGetLastError());
+
+
+  generate_coordinates<<<BlocksPerGrid, threadsPerBlock>>>(devStates, 
+                                                                    x, y ,ITER);
+  checkerror(cudaGetLastError());
+
+  monte_carlo<<<BlocksPerGrid, threadsPerBlock>>>(x, y ,ITER);
+  checkerror(cudaGetLastError());
   /*
    int niter=0;
    double x,y;
@@ -47,5 +107,8 @@ int main(int argc, char* argv)
    pi=(double)count/niter*4;
    printf("# of trials= %d , estimate of pi is %g \n",niter,pi);
    */
+   cudaFree(devStates);
+   cudaFree(x);
+   cudaFree(y);
    return 0;
 }
